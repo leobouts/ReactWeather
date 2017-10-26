68db22ce8f846fdb8018230214fddf01a1777dc0
leobouts <leobouts@gmail.com>
2017-10-26 22:25:52 +0300

    routes

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = React.createClass({
+    
+    render: function () {
+        return (
+            <div>
+
+            <Nav/>
+            <h2>Main comp</h2>
+            
+            </div>
+        );
+    }
+});
+
+module.exports = Main;
