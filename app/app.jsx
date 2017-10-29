36a483bb658abdee1daa32f656ae1b4fc57274de
leobouts <leobouts@gmail.com>
2017-10-30 01:32:34 +0200

    css -> scss

--- app/app.jsx
+++ app/app.jsx
@@ -11,7 +11,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 //load app css
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
 
